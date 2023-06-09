f8b9cfd213e9eb0a898bb019ef7c1b986dbfe909
LittleNora <nguyenduyquangvinh2906@gmail.com>
2023-06-09 16:09:03 +0700

    vinh commit

--- resources/views/pages/subjects/question/list.blade.php
+++ resources/views/pages/subjects/question/list.blade.php
@@ -434,7 +434,8 @@
 @endsection
 
 @section('page-script')
-
+    <script src="https://polyfill.io/v3/polyfill.min.js?features=es6"></script>
+    <script id="MathJax-script" async src="https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-mml-chtml.js"></script>
     <script>
         let url = "/admin/subject/question/{{$id}}?";
         const _token = "{{ csrf_token() }}";
